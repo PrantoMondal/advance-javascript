6134fd9ea7d5ac946bf4d74ef5cf574e1e72b2dc
pranto <kumar.pranto@northsouth.edu>
2021-03-31 23:05:04 +0600

    truthy and falsy

---
+++ true-false.js
@@ -0,0 +1,41 @@
+const age = 30;
+if(age){
+    console.log("Condition is true");
+}
+else{
+    console.log("Condition is false");
+}
+
+const name = "Solaiman";
+if(name){
+    console.log("Condition is true");
+}
+else{
+    console.log("Condition is false");
+}
+
+
+
+
+
+//falsy
+//const name = "";
+//0
+//undefined
+//null
+//NaN
+//Truthy
+//'0', ' '
+
+
+
+/*let name  = NaN;
+console.log(name);
+if(name){
+    console.log("Condition is true");
+
+}
+else{
+    console.log("Condition is false");
+
+}*/
