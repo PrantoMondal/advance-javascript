af5d8e0c65cd9d4b6068c54521fc814ca7914477
pranto <kumar.pranto@northsouth.edu>
2021-04-01 23:53:27 +0600

    split the array

---
+++ arraysplit.js
@@ -0,0 +1,12 @@
+const num = [0,1,2,3,4,5,6];
+// const part = num.slice(2,5);
+// console.log(part);
+
+
+const remove = num.splice(2,5);
+// console.log(remove);
+// console.log(num);
+
+
+const together = num.join(" ");
+console.log(together);
