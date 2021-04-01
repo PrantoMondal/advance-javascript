2cc4c595144fe71246addc7d71df0de4278cb008
pranto <kumar.pranto@northsouth.edu>
2021-04-01 23:17:24 +0600

    == vs===

---
+++ double vs triple equal.js
@@ -0,0 +1,12 @@
+const first = 4;
+const second = "4";
+if (first === second){
+    console.log("condition is True")
+}
+else{
+    console.log("condition is false");
+}
+
+
+//== value check 
+//=== value type change kre
